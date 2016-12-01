0b07e6a1cfc9ade1e5283343c5fffa343164fc73
nikhil2324 <nikhil2324@g.ucla.edu>
2016-11-30 16:11:02 -0800

    Removed unecessary and minified js/css files

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -1,9 +1,2 @@
 module StocksHelper
-	def cache_key_for_stock(stock)
-    	"stock-#{stock.updated_at}"
-  	end
-
-  	def cache_key_for_stock_list
-		"stock-list-#{Stock.maximum(:updated_at)}-#{Stock.maximum(:created_at)}"
-  	end
 end
